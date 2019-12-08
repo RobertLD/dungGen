226dc8a5833c381353fe57f02846f64bdca34711
Robert DeRienzo <Robert.DeRienzo@knights.ucf.edu>
2019-12-08 17:40:57 -0500

    header edit

--- __modules__/printHeader.py
+++ __modules__/printHeader.py
@@ -4,11 +4,20 @@ import os
 from colorama import init
 from colorama import Fore, Back, Style
 import importlib
+import configparser
 
 # gets the current path of installation to assist with opening/reading files
 fileDir = os.path.dirname(os.path.abspath(__file__))
 parentDir = os.path.dirname(fileDir)
+# open the header text file
 headerFile = open(fileDir + "\\resources\\header", "r")
+
+# open the config file
+config = configparser.ConfigParser()
+configFile = config.read("..\\mainConfig.ini")
+
+for line in headerFile:
+    fout.write(line.replace("[VERSION]", "Orange"))
 
 # print header funciton
 def printHeader():
